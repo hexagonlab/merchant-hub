ca0142c9e506e5866c2cbe70aed1b97898b312b6
nomadx <puntsag@vision-finance.com>
2023-11-08 15:27:22 +0800

    fix: loader added some query

--- app/(private)/loading.tsx
+++ app/(private)/loading.tsx
@@ -1,4 +1,8 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <div>Loading</div>;
+  return (
+    <div className='flex-grow px-6 xl:px-24 flex flex-col items-center'>
+      <div className='flex-grow py-12'>Loading...</div>
+    </div>
+  );
 }
